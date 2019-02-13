2f8f3f572d6e10367c7d2a1dd78c6cf417638e3b
Benjamin Cooper <bencooper222@gmail.com>
2019-02-13 17:40:16 -0600

    Finished!

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hacker-spotted-detector
+
+Theoretically, you could run this by just installing Zeit Now and running `now` but there are a lot of environment variables that you'll need. Contact me for details.
--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const slack = require('./lib/slack');
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const { parse } = require('url');
 const { createScores } = require('./lib/main');
 const moment = require('moment-timezone');
 
 module.exports = async (req, res) => {
   try {
-    const scores = JSON.stringify(await createScores());
+    const { query } = parse(req.url, true);
+    // console.log(query);
+    const time =
+      query.time == null || query.time < 1388617536 // Jan 1, 2014
+        ? moment().format('X')
+        : query.time;
+    const scores = JSON.stringify(await createScores(time));
     res.end(scores);
   } catch (err) {
-    res.end(JSON.stringify(process.env));
+    res.end(JSON.stringify(err));
   }
 };
